2f9128f3ef00adf54bb2b9642bd32b819d6e6f79
AM Antor <creativefreelancerantor@gmail.com>
2024-08-27 14:46:55 +0600

    add profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,10 +3,17 @@ import { Link } from "react-router-dom";
 import Loader from "../loader/Loader";
 
 const Profile = () => {
-  const [data, setData] = React.useState(null);
+  const data = {
+    name: "John Doe",
+    designation: "Software Engineer",
+    email: "admin@gmail.com",
+    phone: "1234567890",
+    address: "123 Main St, Anytown USA",
+    avatar: "/img/avatar.png",
+  };
   return (
     <div>
-      <div className="w-full flex items-center justify-between">
+      <div className="w-full flex flex-col md:flex-row items-start md:items-center md:justify-between">
         <div>
           <h1 className="text-xl font-bold">Profile</h1>
           <p className="text-sm text-gray-500">
@@ -14,16 +21,43 @@ const Profile = () => {
           </p>
         </div>
         <div>
-          {!data ? (
-            <Link to={"/add-slider"}>
-              <button className="bg-[#199bff] text-white px-4 py-2 rounded-md">
-                Update Profile
-              </button>
-            </Link>
-          ) : null}
+          <Link to={"/profile/add-profile"}>
+            <button className="bg-[#199bff] text-white px-4 py-2 rounded-md mt-2 md:mt-0">
+              Update Profile
+            </button>
+          </Link>
         </div>
       </div>
-      {data ? <></> : <Loader />}
+      {data ? (
+        <>
+          <div className="mt-5 flex flex-col md:flex-row items-center justify-between gap-5">
+            <div className="w-full md:w-1/3 shadow-md rounded-md p-4 flex flex-col items-center">
+              <img src={data.avatar} alt="" />
+              <h1 className="text-xl font-bold">{data.name}</h1>
+              <p className="text-sm text-gray-500">{data.designation}</p>
+            </div>
+            <div className="w-full md:w-2/3 shadow-md rounded-md p-4">
+              <h1 className="text-xl font-bold mb-4">Contact Details</h1>
+              <p className="text-gray-500 text-md">
+                <span className="font-bold text-black">Email:</span>{" "}
+                {data.email}
+              </p>
+              <hr className="my-2" />
+              <p className="text-gray-500 text-md">
+                <span className="font-bold text-black">Phone:</span>{" "}
+                {data.phone}
+              </p>
+              <hr className="my-2" />
+              <p className="text-gray-500 text-md">
+                <span className="font-bold text-black">Address:</span>{" "}
+                {data.address}
+              </p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <Loader />
+      )}
     </div>
   );
 };
